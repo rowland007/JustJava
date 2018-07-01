74fe259729f98ad7c77bc95510940793408528e9
Randy Rowland <randy.rowland@disroot.org>
2018-07-01 11:11:25 -0400

    Add increase/decrease button and functions

--- app/src/main/java/com/randarlabs/justjava/MainActivity.java
+++ app/src/main/java/com/randarlabs/justjava/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+
+    int numberOfCoffees = 0;
+    double pricePerCoffee = 5.00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,31 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
+        displayPrice(numberOfCoffees * pricePerCoffee);
+        if(numberOfCoffees == 0) {
+            Toast.makeText(getApplicationContext(), "Nothing ordered", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(),"Thank you!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void increaseQuantity(View view) {
+        numberOfCoffees = numberOfCoffees + 1;
+        display(numberOfCoffees);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void decreaseQuantity(View view) {
+        if (numberOfCoffees <= 0) {
+            Toast.makeText(getApplicationContext(),"ERROR: Can't have less than 0", Toast.LENGTH_SHORT).show();
+        }
+        numberOfCoffees = numberOfCoffees > 0 ? numberOfCoffees - 1 : 0;
+        display(numberOfCoffees);
     }
 
     /**
@@ -36,10 +64,9 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
+    private void displayPrice(double number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_value);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
-    private int numberOfCoffees = 2;
 }
